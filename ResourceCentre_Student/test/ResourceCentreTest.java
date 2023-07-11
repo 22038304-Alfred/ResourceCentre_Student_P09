db25a8d98acfae951385e2d3499ed5557c5e8dd7
22038304 <22038304@LAPTOP-O5G2PFQV>
2023-07-11 11:43:09 +0800

    update viewAllChromebook() in ResourceCentre

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 // C206 AY2023 SEM1 - Eclipse 21-09 JDK16
 
-public class ResourceCentreTest {
+public class ResourceCentreTest  {
 	// prepare test data
 	private Camcorder cc1;
 	private Camcorder cc2;
